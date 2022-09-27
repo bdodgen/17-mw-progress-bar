319d88a2772534e3b0708da365d4bb2c536ed579
Briana Dodgen <brianadodgen@gmail.com>
2022-09-27 09:12:48 +0200

    exercise setup

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+import { ProgressBar } from './ProgressBar';
+
+// @TODO no.1 create new instance of ProgressBar. Explore constructor of ProgressBar class!
+//    Take a body element and append progress bar (element) you just created to it.
+//    You shouldn't pass the whole progress bar to appendChild method, but only specific property
+//    explore class ProgressBar and find out which property it is.
+//
+//    code here:
+
+
+
+// @TODO no.2 once you have done TODO no.1 and you can see the progress bar,
+//    fix missing icon on plus button
